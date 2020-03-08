a26d055cff0321699f065c0993a23cef1f459951
Mai Houa Vue <mhvue4@gmail.com>
2020-03-08 15:39:17 -0500

    tested friends.js with get from apiRoute

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,54 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects.
 //  Each of these objects should roughly follow the format below.
 // ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
+var friendsData= [{
+  name:"Ahmed",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}, 
+{
+    name:"Samantha",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        4,
+        2,
+        2,
+        5,
+        5,
+        3,
+        1,
+        5,
+        3,
+        1
+      ]
+  },
+  {
+    name:"David",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        1,
+        5,
+        1,
+        2,
+        3,
+        3,
+        5,
+        4,
+        4,
+        4
+      ]
+  }
+];
+
+module.exports= friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,22 @@
+var friendsDataJS = require("../data/friends.js");
+console.log(friendsDataJS);
 // Your `apiRoutes.js` file should contain two routes:
 module.exports = function (app){
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 app.get("/api/friends", function(req, res){
-    console.log("hello. I work")
-    // return res.json(friendsData);
+    // console.log("hello. I work")
+    return res.json(friendsDataJS);
 });
 
 //  * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
 app.post("/api/friends", function(req, res) {
-    var newData = req.body;
-    console.log(newData);
-    res.json(newData)
-    // return res.json(survResults);
+    var surveyRes = req.body;
+    
+    // This route will also be used to handle the compatibility logic.
+
+    console.log(surveyRes);
+    res.json(surveyRes);
+
 });
 
 };
-// This route will also be used to handle the compatibility logic.
