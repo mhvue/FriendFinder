78afc1b4b7c96c15844cf84a0d345dec57a9ace1
Mai Houa Vue <mhvue4@gmail.com>
2020-03-08 14:58:51 -0500

    added route for apiroute.js.tested get and post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,18 @@
 // Your `apiRoutes.js` file should contain two routes:
-
+module.exports = function (app){
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
+app.get("/api/friends", function(req, res){
+    console.log("hello. I work")
+    // return res.json(friendsData);
+});
+
+//  * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
+app.post("/api/friends", function(req, res) {
+    var newData = req.body;
+    console.log(newData);
+    res.json(newData)
+    // return res.json(survResults);
+});
+
+};
 // This route will also be used to handle the compatibility logic.
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 require("./app/routing/htmlRoutes.js")(app);
-
+require("./app/routing/apiRoutes.js")(app);
 
 
 app.listen(PORT, function(){
