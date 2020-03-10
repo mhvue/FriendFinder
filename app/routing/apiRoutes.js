a840d5038e7bb447164673fbb81d639eaeecb8e7
Mai Houa Vue <mhvue4@gmail.com>
2020-03-09 23:26:36 -0500

    added comments to explain the loop for reminders

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,8 @@ app.get("/api/friends", function(req, res){
     //this will display all possible friends 
 });
 
-//  * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-app.post("/api/friends", function(req, res) {    
-    // var surveyAns= req.body;
+//  * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+app.post("/api/friends", function(req, res) {  
     //make an empty array to put the suvey answers into this empty array and compared to the rest to do the math 
     var surveyAns= {
         name: req.body.name,
@@ -20,25 +19,28 @@ app.post("/api/friends", function(req, res) {
         scores: req.body.scores
     }
 
-    //loop to parse all the scores
+    //loop to parse all the scores so need surveyAns.scores[i] 
     for(var i = 0; i < surveyAns.scores.length; i++){
        surveyAns.scores[i]= parseInt(surveyAns.scores[i]);
     //    console.log(surveyAns.scores[i]);
     }
     
 
-    // loop to get the scores one at a time from FriendsData
+    // loop to get the scores array from FriendsData  
     for(var j = 0; j < friendsDataJS.length; j++){
-        console.log(friendsDataJS[j].scores);
-        for(var k = 0; k < friendsDataJS[j].scores.length; k++){
+        // console.log(friendsDataJS[j].scores);
+        //need this loop b/c we need to access each individual friendsData.scores value from their array 
+        for(var k = 0; k < friendsDataJS[j].scores.length; k++){ 
+            // console.log(friendsDataJS[j].scores[k]); 
+            //then  will take our survey scores and subtract it from the friendsData scores array, one at a time, use Math.abs to make it an absolute val
            var diff =  Math.abs(surveyAns.scores[k] - friendsDataJS[j].scores[k]);
            console.log(diff);
         }
 
     }
 
-    
-    // This route will also be used to handle the compatibility logic.
+    //need if/else statement to get the match 
+
     friendsDataJS.push(surveyAns);
     // console.log(surveyAns);
     res.json(surveyAns);
