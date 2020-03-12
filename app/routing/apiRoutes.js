60db8c0a5dfe0a5bc948e7c5a4b0e1feaa72339a
Mai Houa Vue <mhvue4@gmail.com>
2020-03-11 22:53:25 -0500

    trying to add a home route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,10 @@ app.get("/api/friends", function(req, res){
     return res.json(friendsDataJS);
 });
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
 //  * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 app.post("/api/friends", function(req, res) {  
     var surveyAns= {
